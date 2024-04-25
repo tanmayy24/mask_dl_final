b777ba1fa61c9d7841bc48f41da9ad668b8ee70d
Tanmay Khandelwal <36072284+rythmm24@users.noreply.github.com>
2024-04-25 18:19:39 -0400

    added path

--- merge_masks.py
+++ merge_masks.py
@@ -13,8 +13,12 @@ def main(args):
         mask = np.load(
             os.path.join(args.data_root, args.split, f"video_{i:05d}", "mask.npy")
         )
+        print(f"Loaded mask befor shape for video {i}: {mask.shape}")
+        if i == 1370:  # Check if it's the 18th video
+            pad_width = ((0, 4), (0, 0), (0, 0))  # Padding shape (to match 22nd mask)
+            mask = np.pad(mask, pad_width, mode='constant')
         all_masks.append(mask)
-        print(f"Loaded mask shape for video {i}: {mask.shape}")
+        print(f"Loaded mask after shape for video {i}: {mask.shape}")
 
     all_masks = np.stack(all_masks)
     torch.save(torch.from_numpy(all_masks), args.output_file)
