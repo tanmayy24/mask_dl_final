89944cca644f9727c54cb03343e2f40c5d61f3d0
Tanmay Khandelwal <36072284+rythmm24@users.noreply.github.com>
2024-04-27 22:01:52 -0400

    added values in config

--- train_simvp.py
+++ train_simvp.py
@@ -5,8 +5,6 @@ from lightning.pytorch.callbacks import ModelCheckpoint, LearningRateMonitor
 from lightning.pytorch.tuner import Tuner
 from trainer.config import DEFAULT_DATA_PATH, SEED
 from trainer.trainer import MaskSimVPModule
-from trainer.simvp_model import DEFAULT_MODEL_CONFIG
-
 
 def list_to_folder_name(l):
     return "-".join([str(x) for x in l])
