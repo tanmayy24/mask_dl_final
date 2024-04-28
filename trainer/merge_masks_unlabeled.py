a8e54ff5e518dcc23750c9444e644bed26559a27
Tanmay Khandelwal <36072284+rythmm24@users.noreply.github.com>
2024-04-28 10:38:59 -0400

    updated slurm job

--- trainer/merge_masks_unlabeled.py
+++ trainer/merge_masks_unlabeled.py
@@ -1,43 +1,28 @@
 import argparse
 import os
-import numpy as np
 import tqdm
-
-def process_batch(batch, output_file, mode='a'):
-    stacked_batch = np.stack(batch)
-    np.save(output_file, stacked_batch, allow_pickle=False, fix_imports=False)
-    if mode == 'a':
-        with open(output_file, 'ab') as f:  # Append mode for file
-            print(stacked_batch.size())
-            np.save(f, stacked_batch)
+import numpy as np
 
 def main(args):
-    all_masks = []
-    batch_size = 100  # Adjust batch size based on your memory capacity
-    current_batch = []
-
-    range_start = 2000
+    all_paths = []
+    range_start = 2000 
     range_end = 14999
     for i in tqdm.tqdm(range(range_start, range_end)):
-        mask_path = os.path.join(args.data_root, f"video_{i:05d}_mask.npy")
-        if os.path.exists(mask_path):
-            mask = np.load(mask_path)
-            current_batch.append(mask)
-            if len(current_batch) >= batch_size:
-                process_batch(current_batch, args.output_file)
-                current_batch = []  # Clear the current batch
-        else:
-            print(f"File not found: {mask_path}")
+        file_path = os.path.join(args.data_root, f"video_{i:05d}_mask.npy")
+        if os.path.exists(file_path):  # Check if the file exists
+            all_paths.append(file_path)
+            print(f"Added path for video {i}: {file_path}")
 
-    # Process any remaining masks
-    if current_batch:
-        process_batch(current_batch, args.output_file)
+    # Save the list of paths to a .npy file
+    np.save(args.output_file, np.array(all_paths))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--data_root", type=str, default="/scratch/rn2214/labeled/")
-    parser.add_argument("--output_file", type=str, default="unlabeled_masks.npy")
+    parser.add_argument(
+        "--output_file", type=str, default="unlabeled_masks.npy"
+    )
 
     args = parser.parse_args()
 
